e0f4e94ada123d5e2d628a7dcdc0da18f7646e89
joq62 <joakim.leche@gmail.com>
2021-01-11 01:28:41 +0100

    working

--- test_src/dbase_cluster_test.erl
+++ test_src/dbase_cluster_test.erl
@@ -40,6 +40,14 @@ start()->
     ?assertEqual(ok,two(DbaseList)),
     ?debugMsg("stop two"),
     
+    ?debugMsg("Start three"),
+    ?assertEqual(ok,three(DbaseList)),
+    ?debugMsg("stop three"),
+
+    ?debugMsg("Start restart"),
+    ?assertEqual(ok,restart(DbaseList)),
+    ?debugMsg("stop restart"),
+    
    
       %% End application tests
     ?debugMsg("Start cleanup"),
@@ -47,6 +55,142 @@ start()->
     ?debugMsg("Stop cleanup"),
 
     ?debugMsg("------>"++atom_to_list(?MODULE)++" ENDED SUCCESSFUL ---------"),
+    ok.
+
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+restart([Dbase0,Dbase1,Dbase2])->
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase0,db_lock,read_all_info,[],2000)),
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase1,db_lock,read_all_info,[],2000)),
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase2,db_lock,read_all_info,[],2000)),
+
+    ?assertMatch(ok,slave:stop(Dbase0)),
+
+    ?assertMatch({badrpc,_},rpc:call(Dbase0,db_lock,read_all_info,[],2000)),
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase1,db_lock,read_all_info,[],2000)),
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase2,db_lock,read_all_info,[],2000)),
+    
+    {ok,HostId}=net:gethostname(),
+    {ok,Dbase0}=slave:start(HostId,dbase0,"-pa db0 -setcookie abc"),
+    AllDbaseEnvs=application:get_all_env(dbase),
+
+    % All slavesw have acces to the code in this test , not the case in real system
+    ?assertMatch([ok,ok,ok,ok,ok,ok,ok],[rpc:call(Dbase0,application,set_env,[dbase,Par,Val])||{Par,Val}<-AllDbaseEnvs]),
+    ?assertMatch(ok,rpc:call(Dbase0,application,start,[dbase],5*5000)),
+ 
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase0,db_lock,read_all_info,[],2000)),
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase1,db_lock,read_all_info,[],2000)),
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase2,db_lock,read_all_info,[],2000)),
+
+ AppSpecs=rpc:call(Dbase0,db_app_spec,read_all,[],2000),
+    AppSpecsId=[AppId||{AppId,_AppVsn,_Type,_Directives,_AppEnvs,_Services}<-AppSpecs],
+
+    ServiceSpecs=rpc:call(Dbase0,db_service_def,read_all,[],2000),  
+    ServicesSpecsId=[SpecId||{SpecId,_ServiceId,_ServiceVsn,_StartCmd,_GitPath}<-ServiceSpecs],
+    PassWd=rpc:call(Dbase0,db_passwd,read_all,[],2000),
+    Servers=rpc:call(Dbase0,db_server,read_all,[],2000),
+      
+    ?assertMatch(["master_100_c1.app_spec",
+		  "dbase_100_c2.app_spec",
+		  "calc_c_100.app_spec",
+		  "master_100_c0.app_spec",
+		  "dbase_100_c0.app_spec",
+		  "dbase_100_c1.app_spec",
+		  "calc_a_100.app_spec",
+		  "calc_b_100.app_spec",
+		  "master_100_c2.app_spec"],
+		 AppSpecsId),
+
+  ?assertMatch(["multi_100.service_spec",
+		"server_100.service_spec",
+		"adder_100.service_spec",
+		"divi_100.service_spec",
+		"common_100.service_spec",
+		"master_100.service_spec",
+		"calc_100.service_spec",
+		"dbase_100.service_spec"],
+	       ServicesSpecsId),
+
+    ?assertMatch([{"joq62","20Qazxsw20"}],
+		 PassWd),
+    ?assertMatch([{"c2","joq62","festum01","192.168.0.202",22,not_available},
+		  {"c1","joq62","festum01","192.168.0.201",22,not_available},
+		  {"c0","joq62","festum01","192.168.0.200",22,not_available}],
+		 Servers),
+
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+three([Dbase0,Dbase1,Dbase2])->
+    ?assertMatch([{"joq62","20Qazxsw20"}],rpc:call(Dbase0,db_passwd,read_all,[],2000)),
+    ?assertMatch([{"joq62","20Qazxsw20"}],rpc:call(Dbase1,db_passwd,read_all,[],2000)),
+    ?assertMatch({badrpc,_},rpc:call(Dbase2,db_passwd,read_all,[],2000)),
+    
+    AllDbaseEnvs=application:get_all_env(dbase),
+
+    % All slavesw have acces to the code in this test , not the case in real system
+    ?assertMatch([ok,ok,ok,ok,ok,ok,ok],[rpc:call(Dbase2,application,set_env,[dbase,Par,Val])||{Par,Val}<-AllDbaseEnvs]),
+    sys_log:msg(misc_oam:masters(),log,
+		["Dbase0 =", Dbase2],
+		?MODULE,?LINE),
+    ?assertMatch(ok,rpc:call(Dbase2,application,start,[dbase],5*5000)),
+
+     %% Test 
+    AppSpecs=rpc:call(Dbase2,db_app_spec,read_all,[],2000),
+    AppSpecsId=[AppId||{AppId,_AppVsn,_Type,_Directives,_AppEnvs,_Services}<-AppSpecs],
+
+    ServiceSpecs=rpc:call(Dbase2,db_service_def,read_all,[],2000),  
+    ServicesSpecsId=[SpecId||{SpecId,_ServiceId,_ServiceVsn,_StartCmd,_GitPath}<-ServiceSpecs],
+    PassWd=rpc:call(Dbase2,db_passwd,read_all,[],2000),
+    Servers=rpc:call(Dbase2,db_server,read_all,[],2000),
+      
+    ?assertMatch(["master_100_c1.app_spec",
+		  "dbase_100_c2.app_spec",
+		  "calc_c_100.app_spec",
+		  "master_100_c0.app_spec",
+		  "dbase_100_c0.app_spec",
+		  "dbase_100_c1.app_spec",
+		  "calc_a_100.app_spec",
+		  "calc_b_100.app_spec",
+		  "master_100_c2.app_spec"],
+		 AppSpecsId),
+
+  ?assertMatch(["multi_100.service_spec",
+		"server_100.service_spec",
+		"adder_100.service_spec",
+		"divi_100.service_spec",
+		"common_100.service_spec",
+		"master_100.service_spec",
+		"calc_100.service_spec",
+		"dbase_100.service_spec"],
+	       ServicesSpecsId),
+
+    ?assertMatch([{"joq62","20Qazxsw20"}],
+		 PassWd),
+    ?assertMatch([{"c2","joq62","festum01","192.168.0.202",22,not_available},
+		  {"c1","joq62","festum01","192.168.0.201",22,not_available},
+		  {"c0","joq62","festum01","192.168.0.200",22,not_available}],
+		 Servers),
+
+    ?assertMatch([{"joq62","20Qazxsw20"}],rpc:call(Dbase0,db_passwd,read_all,[],2000)),
+    ?assertMatch([{"joq62","20Qazxsw20"}],rpc:call(Dbase1,db_passwd,read_all,[],2000)),
+    ?assertMatch([{"joq62","20Qazxsw20"}],rpc:call(Dbase2,db_passwd,read_all,[],2000)),
+
+    %Test distribution
+
+    ?assertMatch({atomic,ok},rpc:call(Dbase0,db_lock,create,[test_lock2,20],2000)),
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase0,db_lock,read_all_info,[],2000)),
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase1,db_lock,read_all_info,[],2000)),
+    ?assertMatch([{test_lock2,20}],rpc:call(Dbase2,db_lock,read_all_info,[],2000)),
+    
     ok.
 
 %% --------------------------------------------------------------------
